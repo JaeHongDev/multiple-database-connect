5c1dae34f2135764dc7bc22c5dbc34d2eea07fc7
JaehongDev <println@kakao.com>
2022-12-15 22:02:55 +0900

    Initial commit

---
+++ src/main/java/com/example/multipledatabaseconnect/MultipleDatabaseConnectApplication.java
@@ -0,0 +1,13 @@
+package com.example.multipledatabaseconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipleDatabaseConnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultipleDatabaseConnectApplication.class, args);
+    }
+
+}
